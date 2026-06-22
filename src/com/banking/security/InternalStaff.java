c688d869c01d5aefe150edf4f24f4cb4979cf2c3
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 00:15:31 -0300

    Permissions class

---
+++ src/com/banking/security/InternalStaff.java
@@ -0,0 +1,18 @@
+public class InternalStaff {
+    
+    private RoleType role;
+    private int permissionLevel;
+    
+    
+    public InternalStaff(RoleType role, int permissionLevel) {
+        this.role = role;
+        this.permissionLevel = permissionLevel;
+    }
+    
+    
+    public void reviewTrasaction(Transaction t) {
+        
+    }
+
+    
+}
