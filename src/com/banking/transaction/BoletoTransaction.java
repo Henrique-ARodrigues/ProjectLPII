15f98430585c77d687effb83d788477b6238f8d5
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 20:21:11 -0300

    Adding methods

--- src/com/banking/transaction/BoletoTransaction.java
+++ src/com/banking/transaction/BoletoTransaction.java
@@ -1,6 +1,39 @@
 package com.banking.transaction;
 
+import java.time.LocalDateTime;
+import com.banking.account.BankAccount;
 
-public class BoletoTransaction {
+public class BoletoTransaction extends Transaction {
     
+    private String barCode;
+
+    public BoletoTransaction(BankAccount source, BankAccount destination, double amount, LocalDateTime timestamp, String barCode) {
+        super(source, destination, amount, timestamp);
+        this.barCode = barCode;
+    }    
+
+
+    // Sobrescreve o método validate para aplicar regra de boletos
+    @Override
+    public boolean validate() {
+        // Regra específica: O código de barras não pode ser nulo ou vazio
+        if (this.barCode == null || this.barCode.trim().isEmpty()) {
+            this.setStatus("FAILED");
+            System.out.println("Transaction FAILED: Invalid or empty Barcode.");
+            return false;
+        }
+        
+        // Se o boleto for válido, chama o antifraude
+        return super.validate();
+    }
+
+    // Getter
+    public String getBarCode() {
+        return barCode;
+    }
+
+    // Setter
+    public void setBarCode(String barCode) {
+        this.barCode = barCode;
+    }
 }
