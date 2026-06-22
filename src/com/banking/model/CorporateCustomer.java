8ac45c1b8c4bf240b92c43a5074ba5f254646bad
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 00:37:54 -0300

    Corporate Customer class

---
+++ src/com/banking/model/CorporateCustomer.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CorporateCustomer extends User {
+    
+    private String cnpj;
+    private String businessName;
+    private String tradeName;
+    private list<Employee> allowedEmployees;
+    
+}
