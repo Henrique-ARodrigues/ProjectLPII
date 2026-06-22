290164e6cdf97cd68d279fe954c1720fb30c7c73
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 00:37:00 -0300

    User class

---
+++ src/com/banking/model/User.java
@@ -0,0 +1,47 @@
+import java.util.UUID;
+
+
+public class User {
+
+    private UUID id;
+    private String email;
+    private String password;
+
+    
+
+    public User(String email, int password) {
+        
+        this.id = UUID.randomUUID();
+        this.email = email;
+        this.password = password ;
+        
+    }
+
+    
+
+    // Getters
+    public String getEmail() {
+        return email;
+    }
+
+    public UUID getId() {
+        return id
+    }
+
+    public int getPassword() {
+        return password ;
+    }
+
+    
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(int password ) {
+        this.password = password ;
+    }
+
+    
+}
