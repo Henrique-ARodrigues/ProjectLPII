776d44769ba090592bf0ee0ae0b3b4df2fd38133
euvih <vitoriakellypdasilva@gmail.com>
2026-06-22 13:43:10 -0300

    feat: implementa VirtualCard conforme UML

--- src/com/banking/card/VirtualCard.java
+++ src/com/banking/card/VirtualCard.java
@@ -1,5 +1,31 @@
-package card;
+package com.banking.card;
 
-public class VirtualCard {
-    
+import com.banking.model.Employee;
+
+public class VirtualCard extends CorporateCard {
+
+    private boolean isSingleUse;
+
+    public VirtualCard(String cardNumber,
+                       String holderName,
+                       String cvv,
+                       String expirationDate,
+                       String status,
+                       double dailyLimit,
+                       Employee holder,
+                       boolean isSingleUse) {
+
+        super(cardNumber, holderName, cvv, expirationDate,
+              status, dailyLimit, holder);
+
+        this.isSingleUse = isSingleUse;
+    }
+
+    public boolean isSingleUse() {
+        return isSingleUse;
+    }
+
+    public void setSingleUse(boolean singleUse) {
+        isSingleUse = singleUse;
+    }
 }
