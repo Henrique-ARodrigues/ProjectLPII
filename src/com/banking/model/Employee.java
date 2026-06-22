8a483d5536388b2d38d2080bd616000cc25067cf
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 00:35:28 -0300

    Employee class

---
+++ src/com/banking/model/Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+    
+    private String name;
+    private String cpf;
+    private String department;
+    
+    
+    public Employee(String name. String cpf, String department) {
+        this.name = name;
+        this.cpf = cpf;
+        this.department = department;
+    }
+    
+    
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDepartment(String department) {
+        private String department;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+}
