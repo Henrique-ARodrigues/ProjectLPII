40af7ccb4c3877195062052b6ce98cd8a4ef9c9f
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 01:17:19 -0300

    Adding method that charges fees

--- src/com/banking/account/InternationalAccount.java
+++ src/com/banking/account/InternationalAccount.java
@@ -1,20 +1,48 @@
-public class InternatonalAccount extends BankAccount {
+package com.banking.account;
+
+import com.banking.customer.CorporateCustomer;
+
+public class InternationalAccount extends BankAccount {
+    
     private String currency;
-    private double exchangeRate;
+    private double exchangeRate; 
     
-    
-    public InternatonalAccount(String currency, double exchangeRate, CorporateCustomer owner, double balance, String agency, String accountNumber) {
-        
+    public InternationalAccount(String currency, double exchangeRate, CorporateCustomer owner, double balance, String agency, String accountNumber) {
         super(owner, balance, agency, accountNumber);
         this.currency = currency;
         this.exchangeRate = exchangeRate;
-        
     }
     
+    
+    //Calcula as taxas da conta internacional.
+     //Taxa de conversão/manutenção de 1% sobre o saldo da conta.
     
     @Override
     public double calculateFees() {
-        
+        // Pega o saldo usando o getBalance() da classe mãe e aplica 1% (0.01)
+        return this.getBalance() * 0.01;
+    }
+
+    
+    
+
+    // Getters
+    public double getBalanceInForeignCurrency() {
+        return this.getBalance() / this.exchangeRate;
+    }
+    public String getCurrency() {
+        return currency;
+    }
+    public double getExchangeRate() {
+        return exchangeRate;
     }
     
+    
+    // Setters
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
 }
