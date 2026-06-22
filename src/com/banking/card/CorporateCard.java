bb22bd037296d0a2243d7f68e96fe152c8664e6e
euvih <vitoriakellypdasilva@gmail.com>
2026-06-22 12:52:41 -0300

    feat: implementa classes de cartoes

--- src/com/banking/card/CorporateCard.java
+++ src/com/banking/card/CorporateCard.java
@@ -1,5 +1,82 @@
-package card;
+package com.banking.card;
+
+import com.banking.model.Employee;
 
 public class CorporateCard {
-    
+
+    private String cardNumber;
+    private String holderName;
+    private String cvv;
+    private String expirationDate;
+    private String status;
+    private double dailyLimit;
+    private Employee holder;
+
+    public CorporateCard(String cardNumber, String holderName, String cvv,
+                         String expirationDate, String status, double dailyLimit,
+                         Employee holder) {
+        this.cardNumber = cardNumber;
+        this.holderName = holderName;
+        this.cvv = cvv;
+        this.expirationDate = expirationDate;
+        this.status = status;
+        this.dailyLimit = dailyLimit;
+        this.holder = holder;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getDailyLimit() {
+        return dailyLimit;
+    }
+
+    public Employee getHolder() {
+        return holder;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDailyLimit(double dailyLimit) {
+        this.dailyLimit = dailyLimit;
+    }
+
+    public void setHolder(Employee holder) {
+        this.holder = holder;
+    }
 }
