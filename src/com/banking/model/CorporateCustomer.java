a2ba18456f7e0d7098aaa54a453cb3b0a387aeb2
Henrique <henriquealvesrodrigues124@gmail.com>
2026-06-22 01:17:55 -0300

    Corporate Customer class

--- src/com/banking/model/CorporateCustomer.java
+++ src/com/banking/model/CorporateCustomer.java
@@ -9,4 +9,47 @@ public class CorporateCustomer extends User {
     private String tradeName;
     private list<Employee> allowedEmployees;
     
+    
+    public CorporateCustomer(String name, String email, String cnpj, String businessName, String tradeName) {
+        super(name, email); // Supondo que seu User receba name e email no construtor
+        this.cnpj = cnpj;
+        this.businessName = businessName;
+        this.tradeName = tradeName;
+        this.allowedEmployees = new ArrayList<>(); // Inicializa a lista vazia
+    }
+    
+    // Método para autorizar um novo funcionário na conta da empresa
+    public void addAllowedEmployee(Employee employee) {
+        this.allowedEmployees.add(employee);
+    }
+    
+    
+    // Getters
+    public String getCnpj() {
+        return cnpj;
+    }
+    public String getBusinessName() {
+        return businessName;
+    }
+    public String getTradeName() {
+        return tradeName;
+    }
+    public List<Employee> getAllowedEmployees() {
+        return allowedEmployees;
+    }
+    
+    
+    // Setters
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+    public void setTradeName(String tradeName) {
+        this.tradeName = tradeName;
+    }
+    
 }
+
+
